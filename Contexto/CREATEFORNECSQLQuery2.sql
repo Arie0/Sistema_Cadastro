3a6f8c3e3d70c78c4dc8623dc2865ddb716e5d37
Ariele <87340743+Arie0@users.noreply.github.com>
2025-06-02 08:55:17 -0300

    Add files via upload

---
+++ Contexto/CREATEFORNECSQLQuery2.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Fornecedores (
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    Nome NVARCHAR(100) NOT NULL,
+    CNPJ NVARCHAR(18) UNIQUE NOT NULL,
+    Telefone NVARCHAR(20),
+    Email NVARCHAR(100),
+    Endereco NVARCHAR(255)
+);
---
+++ Contexto/CREATERUSERSQLQuery4.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Usuarios (
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100),
+  
+);
